4e2c6e3290c9feb980f6f9bf7a38910839d1c955
abigailsylvie <abigailsylvie78@gmail.com>
2022-08-15 14:48:38 +0100

    example of commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string>
+#include<ostream>
+#include<sstream>
+#include "color.h"
+#include "card.h"
+#include "messages.h"
+#include "utils.h"
+#include "game.h"
+using namespace std;
+
+int main()
+{
+    Game *Game;
+    Game->StartGame();
+    system("pause > 0");
+    return 0;
+}
